b404835f3f4c11a496254a95a60e3f5ec4f80e5b
OuSS <ouss.cs@gmail.com>
2018-11-19 16:37:03 +0100

    Initial Commit

---
+++ PanGestureSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PanGestureSample
+//
+//  Created by OuSS on 11/19/18.
+//  Copyright © 2018 OuSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
