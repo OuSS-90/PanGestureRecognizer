9379bac482e35290e8c9b29eb2aa1c56af6ebe26
OuSS <ouss.cs@gmail.com>
2018-11-19 17:13:07 +0100

    Pan gesture example

--- PanGestureSample/ViewController.swift
+++ PanGestureSample/ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var docImage: UIImageView!
+    @IBOutlet weak var trashImage: UIImageView!
+    
+    var docOrigin: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setUpPanGesture(view: docImage)
+        view.bringSubviewToFront(docImage)
+        
+        docOrigin = docImage.frame.origin
+    }
+    
+    func setUpPanGesture(view: UIView) {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(self.handlePan(_:)))
+        view.addGestureRecognizer(pan)
     }
 
+    @objc func handlePan(_ sender: UIPanGestureRecognizer) {
+        guard let docView = sender.view else { return }
+        
+        switch sender.state {
+        case .began, .changed:
+            movingDocImage(view: docView, sender: sender)
+        case .ended:
+            if docView.frame.intersects(trashImage.frame) {
+                deleteDocImage()
+            } else {
+                backDocImageToOrigin()
+            }
+        default:
+            break
+        }
+    }
+    
+    func movingDocImage(view: UIView, sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        view.frame.origin = CGPoint(x: view.frame.origin.x + translation.x, y: view.frame.origin.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
+    }
+    
+    func backDocImageToOrigin() {
+        UIView.animate(withDuration: 0.3) {
+            self.docImage.frame.origin = self.docOrigin
+        }
+    }
+    
+    func deleteDocImage() {
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: [], animations: {
+           self.docImage.alpha = 0
+        }, completion: { (finished) in
+            self.trashImage.image = UIImage(named: "Trash")
+        })
+    }
 
 }
 
